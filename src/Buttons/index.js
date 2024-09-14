92dfa94103b226a08b092611a0f463b711d4ee16
OrangeBlue342 <joannaczub53@gmail.com>
2024-09-14 19:22:52 +0200

    changes undoes props

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./style.css";
 
-const Buttons = (tasks, hideDoneTasks, togglehideDoneTasks) => {
-if (tasks.lenght === 0) {
+const Buttons = (props) => {
+if (props.tasks.lenght === 0) {
     return null
 }
 
     return (
         <div className="buttons">
-             <button onClick={togglehideDoneTasks} className="buttons_button">
-        {hideDoneTasks ? "Pokaż" : "Ukryj"} skończone zadania</button>
+             <button className="buttons_button">
+        {props.hideDoneTasks ? "Pokaż" : "Ukryj"} skończone zadania</button>
         <button className="buttons_button"
-        disabled={tasks.every (({ done }) => done)}
+        disabled={props.tasks.every (({ done }) => done)}
         >
         Oznacz wszystkie jako ukończone
         </button>
