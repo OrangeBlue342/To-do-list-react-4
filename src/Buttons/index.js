c821b92e1e2d565b8e418517e1c70b061b876498
OrangeBlue342 <joannaczub53@gmail.com>
2024-09-24 10:48:02 +0200

    Buttons updated

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -8,11 +8,11 @@ if (tasks.lenght === 0) {
 
     return (
         <div className="buttons">
-             <button onClick={togglehideDoneTasks} className="buttons_button">
+             <button onClick={togglehideDoneTasks} className="buttons_button1">
         {hideDoneTasks ? "Pokaż" : "Ukryj"} skończone zadania</button>
         <button 
         onClick={() => setAllDone()}
-        className="buttons_button"
+        className="buttons_button2"
         disabled={tasks.every (({ done }) => done)}
         >
         Oznacz wszystkie jako ukończone
